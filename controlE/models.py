8c516e63002f3912328b0916358002dd5b3fdab3
Jorge Pablo Ferreyra <12859442+Ferreyrajp@users.noreply.github.com>
2023-07-05 21:28:40 -0300

    actualizada la presentacion de la BD

--- controlE/models.py
+++ controlE/models.py
@@ -8,11 +8,17 @@ class Clientes(models.Model):
   telefono = models.CharField(max_length=15)
   email = models.EmailField()
 
+  def __str__(self):
+        return self.nombre
+
 class Empleados(models.Model):
     nombre = models.CharField(max_length=30)
     direccion = models.CharField(max_length=50)
     telefono = models.CharField(max_length=15)
     email= models.EmailField()
+
+    def __str__(self):
+        return self.nombre
 
 class Reparaciones(models.Model):
     cliente_id = models.ForeignKey(Clientes, on_delete=models.CASCADE)
@@ -20,8 +26,15 @@ class Reparaciones(models.Model):
     descripcion = models.CharField(max_length=200)
     fecha_recepcion = models.DateTimeField(auto_now_add=True)
     estado = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Reparaciones ID: {self.pk}, Clientes Nombre: {self.cliente_id.nombre}"
   
 class Asignaciones(models.Model):
   reparacion_id = models.ForeignKey(Reparaciones, on_delete=models.CASCADE)
   empleado_id = models.ForeignKey(Empleados, on_delete=models.CASCADE)
   fecha_asignacion=  models.DateTimeField(auto_now_add=True)
+
+  def __str__(self):
+        debolver = f"Asignaciones ID: {self.pk}, Reparaciones Equipo: {self.reparacion_id.equipo},Asignaciones ID: {self.pk}, Empleados Nombre: {self.empleado_id.nombre}"
+        return (debolver)
